a62422c302dc52a1da517aa6001cccb944362dbd
Tracy Smith <tracysmith@Tracys-MacBook-Pro.local>
2022-02-21 23:05:36 +0000

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,10 +9,16 @@ const Contact = () => {
 				<a className="contact-button" href="mailto:tracysmith32@live.co.uk">
 					Email Me
 				</a>
-				<a href="https://github.com/tracysuzanne321">
+				<a
+					href="https://github.com/tracysuzanne321"
+					target="_blank"
+					rel="noreferrer">
 					<img src={github} alt="GitHub Logo" />
 				</a>
-				<a href="https://www.linkedin.com/in/tracysuzanne32/">
+				<a
+					href="https://www.linkedin.com/in/tracysuzanne32/"
+					target="_blank"
+					rel="noreferrer">
 					<img src={linkedin} alt="Linkedin Logo" />
 				</a>
 			</div>
